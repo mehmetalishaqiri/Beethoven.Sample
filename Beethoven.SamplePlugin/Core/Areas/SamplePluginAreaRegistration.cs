5695f3427ab2934c5eda4c521d7f304d857dddf4
Mehmetali Shaqiri <mehmetalishaqiri@gmail.com>
2015-03-17 16:43:03 +0100

    no message

--- Beethoven.SamplePlugin/Core/Areas/SamplePluginAreaRegistration.cs
+++ Beethoven.SamplePlugin/Core/Areas/SamplePluginAreaRegistration.cs
@@ -1,25 +1,25 @@
-﻿/*
- * DeepThought.SamplePlugin
- * 
- * Written for .NET 4.0 in C#
- * 
- * Copyright (C) 2009, 2010, 2011. All Right Reserved, Spartansoft L.L.C.
- * http://spartansoft.org
- * 
- * Proprietary and Confidential information of Spartansoft L.L.C. 
- * Redistribution and use in source and binary forms, with or without modification, 
- * without the authorization of Spartansoft L.L.C.  is prohibited. * 
- * 
- * "Whatever happens SPARTAN's code must stand ... or at least crash responsibly."
- *  
- * File Name: SamplePluginAreaRegistration.cs
- * 
- * File Authors:
- * 		Mehmetali N. Shaqiri, m.shaqiri@spartansoft.org
- * 
- * Date Created:
- *      31.07.2011 06:32 PM
+﻿/*  
+    The MIT License (MIT)
+    
+    Copyright (C) 2011 Mehmetali Shaqiri (mehmetalishaqiri@gmail.com)
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE. 
  */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Beethoven.Sample
-A sample plugin based web application build using Beethoven
+## Beethoven.Sample
+A sample plugin based web application to demonstrate the usage of Beethoven and Bethoven.Plugins
+
+## Dependencies
+* [Beethoven <img src="https://img.shields.io/nuget/v/Beethoven.svg">](https://www.nuget.org/packages/Beethoven)
+* [Beethoven.Plugins <img src="https://img.shields.io/nuget/v/Beethoven.Plugins.svg">](https://www.nuget.org/packages/Beethoven.Plugins)
